73a4c12188a7bda5e7b1845abf3727f8f4bdf81c
Mohamed Sayed Elhagry <m.elhagry@ieee.org>
2017-10-06 17:05:20 +0200

    Add files via upload

---
+++ Solu1/App1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Please Enter Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);*/
+            /*string x = (10).ToString();
+            Console.WriteLine(x + 5);*/
+
+            /*Console.WriteLine("Please Enter Your num1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Your num2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Sum = " + (num1 + num2));*/
+
+            /*Console.WriteLine("Please Enter Your num1");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Your num2");
+            int num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("The Sum = " + (num1 + num2));*/
+
+            int num1, num2;
+            Console.WriteLine("Please Enter Your num1");
+            Int32.TryParse(Console.ReadLine(), out num1);
+            Console.WriteLine("Please Enter Your num2");
+            Int32.TryParse(Console.ReadLine(), out num2);
+            Console.WriteLine("The Sum = " + (num1 + num2));
+
+        }
+    }
+}
---
+++ Solu1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.x = new System.Windows.Forms.TextBox();
+            this.y = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(96, 103);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "ok";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(12, 44);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(100, 20);
+            this.x.TabIndex = 1;
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(172, 44);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(100, 20);
+            this.y.TabIndex = 2;
+            // 
+            // sum
+            // 
+            this.sum.AutoSize = true;
+            this.sum.Location = new System.Drawing.Point(117, 165);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(0, 13);
+            this.sum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Num1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Num2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.btn_ok);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.Label sum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
